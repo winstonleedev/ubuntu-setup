121dbdf248d3cf11cde30bb3505a57e53241fce4
Thanh Phu <git@thanhphu.net>
2020-01-08 16:55:22 +0900

    New virtualbox version

--- setup.sh
+++ setup.sh
@@ -85,7 +85,7 @@ wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian bionic contrib"
 sudo apt update
-sudo apt install -y virtualbox-6.0
+sudo apt install -y virtualbox-6.1
 
 sudo apt install -y libqt5svg5 qml-module-qtquick-controls
 
